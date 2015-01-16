681e214a6e1e118d7990ba352090eef78e190930
liuyunxuan <user@userdeMacBook-Air.local>
2015-01-16 21:51:49 +0800

    Initial Commit

---
+++ mapPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mapPicture
+//
+//  Created by liuyunxuan on 15/1/16.
+//  Copyright (c) 2015年 liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mapPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mapPicture
+//
+//  Created by liuyunxuan on 15/1/16.
+//  Copyright (c) 2015年 liuyunxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
